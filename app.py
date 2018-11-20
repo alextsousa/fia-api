588e4d9e31f34bdd0b19d49dc3bd0b5f9a838353
Alexander Tele de Sousa <alex-sousa@hotmail.com>
2018-11-20 17:13:36 -0200

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,17 @@
+#Usage: python app.py
 import os
-from flask import Flask, jsonify, request
+ 
+from flask import Flask, render_template, request, redirect, url_for
+from werkzeug import secure_filename
+from keras.preprocessing.image import ImageDataGenerator, load_img, img_to_array
+from keras.models import Sequential, load_model
+import numpy as np
+import argparse
+import imutils
+import cv2
+import time
+import uuid
+import base64
 
 app = Flask(__name__)
 
@@ -8,5 +20,5 @@ def index():
     return "A nossa API está <span style='color: green; font-size: 30px'>online</span> em /api/"
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))
-    app.run(host='0.0.0.0', port=port, debug=True)
+    app.debug=true
+    app.run(host='0.0.0.0', port=3000)
